22be48d20549a901787dba9285bef6398d41b1c6
kaybao062 <116699898+kaybao062@users.noreply.github.com>
2023-11-12 22:41:08 +0000

    xz

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,45 @@
 class UsersController < ApplicationController
 
-  def index
+  def all_users
     matching_users = User.all
     @users = matching_users.order(:created_at)
 
     render({ :template => "users_templates/all_users"})
   end
   
-  def show
+  def users_details
     username = params.fetch("username")
     matching_users = User.where({ :username => username })
     @user = matching_users.at(0)
 
-    render({ :template => "user_templates/user_details"})
+    render({ :template => "users_templates/user_details"})
   end
   
   def create
-    user = User.new
-    user.username = params.fetch("query_username")
-    user.save
+    @user = User.new
+    @user.username = params.fetch("query_username")
+    @user.save
     
-    redirect_to("/users/#{user.username}")
+    if @user.valid?
+      @user.save
+      redirect_to("/users/#{@user.username}", { :notice => "user created successfully." })
+    else
+      redirect_to("/users/#{@user.username}", { :notice => "user failed to create successfully." })
+    end
   end
   
   def update
-    user_id = params.fetch("user_id")
-    matching_users = User.where({ :id => user_id })
-    the_user = matching_users.at(0)
+    the_name = params.fetch("user_name")
+    matching_users = User.where({ :username => the_name })
+    @user = matching_users.at(0)
     
-    the_user.username = params.fetch("query_username")
-    the_user.save
-    redirect_to("/users/#{user.username}")
+    @user.username = params.fetch("query_username")
+    if @user.valid?
+      @user.save
+      redirect_to("/users/#{@user.username}", { :notice => "User updated successfully."} )
+    else
+      redirect_to("/users/#{@user.username}", { :notice => "User updated unsuccessfully."} )
+    end
   end
 
 end
